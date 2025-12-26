12cc7d20e0bcc5582f80c5bf26e7212417aec232
Iaak12 <ayazzkh1226@gmail.com>
2025-12-26 23:30:17 +0530

    first commit

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TopBar from '../Layout/TopBar'
+
+const Header = () => {
+  return (
+    <>
+    {/* Top Bar */}
+    <TopBar />
+    {/* Navbar */}
+    {/* Cart Drawer */}
+
+    </>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Layout/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopBar = () => {
+  return (
+    <div  className="bg-[#ea2e0e]">
+
+    </div>
+  )
+}
+
+export default TopBar
