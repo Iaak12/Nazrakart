92447c080ffe81bbe24dc7403aa63544d57d4737
Iaak12 <ayazzkh1226@gmail.com>
2025-12-27 08:31:13 +0530

    navbar completed

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import TopBar from '../Layout/TopBar'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <>
+    <header>
     {/* Top Bar */}
     <TopBar />
     {/* Navbar */}
+    <Navbar />
     {/* Cart Drawer */}
 
-    </>
+    </header>
   )
 }
 
